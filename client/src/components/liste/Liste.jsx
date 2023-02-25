31c8f28e3a5627665529d2ebad98a4f1b6c553ed
xmeix <lamiaboualouache@gmail.com>
2023-02-25 18:28:29 +0100

    validate

--- client/src/components/liste/Liste.jsx
+++ client/src/components/liste/Liste.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import BoiteRecherche from "./boiteRecherche/BoiteRecherche";
 import "./Liste.css";
 import Row from "./row/Row";
-const Liste = ({ title,search }) => {
+const Liste = ({ title, search }) => {
+  const [selectedOption, setSelectedOption] = useState("");
+  const options = ["etat", "moyen de transport"];
+  const data = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+    { name: "Bob", age: 40 },
+  ];
+
+  const filteredData = data.filter((item) => {
+    //selectedOption ? item.name === selectedOption : true;
+  });
   return (
     <div className="liste">
       <p className="listTitle">{title}</p>
       <div className="outils">
-        <BoiteRecherche search={search}/>
+        <BoiteRecherche search={search} />
+
+        <select
+          className="selectBox"
+          value={selectedOption}
+          onChange={(e) => setSelectedOption(e.target.value)}
+        >
+          <option value="">filter: Select an option</option>
+          {options.map((option, index) => (
+            <option key={index} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
       </div>
       <ul className="rows">
         <li>
--- client/src/components/liste/boiteRecherche/BoiteRecherche.jsx
+++ client/src/components/liste/boiteRecherche/BoiteRecherche.jsx
@@ -1,10 +1,14 @@
 import "./BoiteRecherche.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
-const BoiteRecherche = ({search}) => {
+const BoiteRecherche = ({ search }) => {
   return (
     <div className="boiteRecherche">
       <SearchRoundedIcon className="searchIcon" />
-      <input placeholder={`type your ${search} here` }className="searchInput" />
+      <input
+        type="search"
+        placeholder={`type your ${search} here`}
+        className="searchInput"
+      />
     </div>
   );
 };
