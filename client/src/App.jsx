1f5a9361bcac42dabb1f9e56c8dfbcc33049d4f7
xmeix <lamiaboualouache@gmail.com>
2023-02-25 16:08:05 +0100

    fixedPageName

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <NavBar /> 
       <Routes>
           {/* <Route path="/" element={<LoginPage/>}/> */}
-          <Route path="/gm" element={<GestionMission/>}/> 
+          <Route path="/gestion-des-mission" element={<GestionMission/>}/> 
         </Routes>  
         </div>
   );
