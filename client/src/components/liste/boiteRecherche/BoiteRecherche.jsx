ab9f22c938207717b519ac8b6384867a866e80cb
xmeix <lamiaboualouache@gmail.com>
2023-02-25 21:34:41 +0100

    fixed searchbar distance

--- client/src/components/liste/boiteRecherche/BoiteRecherche.jsx
+++ client/src/components/liste/boiteRecherche/BoiteRecherche.jsx
@@ -1,9 +1,15 @@
 import "./BoiteRecherche.css";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import { InputAdornment } from "@mui/material";
 const BoiteRecherche = ({ search }) => {
   return (
     <div className="boiteRecherche">
-      <SearchRoundedIcon className="searchIcon" />
+      <InputAdornment
+        position="start"
+        style={{ position: "absolute", top: "1.1em", left: "0.5em" }}
+      >
+        <SearchRoundedIcon className="searchIcon" />
+      </InputAdornment>
       <input
         type="search"
         placeholder={`type your ${search} here`}
