64b386ee6ae9e3ad641e92be8d78bc0336c87ba7
xmeix <lamiaboualouache@gmail.com>
2023-02-26 22:04:16 +0100

    GSR  terminé

--- client/src/pages/gestionEmployes/GestionEmploye.jsx
+++ client/src/pages/gestionEmployes/GestionEmploye.jsx
@@ -9,10 +9,11 @@ const GestionEmploye = () => {
     { label: "Nom", type: "text", width: "80%" },
     { label: "Prénom", type: "text", width: "80%" },
     { label: "Fonction", type: "text", width: "80%" },
-    { label: "Numéro de téléphone", type: "tel", width: "80%" },
-    { label: "Adresse", type: "text", width: "150%" },
     { label: "Date de naissance", type: "date", width: "80%" },
     { label: "Lieu de naissance", type: "text", width: "80%" },
+    { label: "Adresse", type: "text", width: "80%" },
+
+    { label: "Numéro de téléphone", type: "tel", width: "80%" },
     { label: "Email", type: "email", width: "80%" },
     { label: "Mot de passe", type: "password", width: "80%" },
   ];
@@ -24,37 +25,29 @@ const GestionEmploye = () => {
         <div className="form">
           <div className="formTitle">Formulaire d'ajout d'employé</div>
           <div className="inputs">
-            {" "}
             <div className="inside">
               {entries.map((entry, i) => {
-                console.log(entry);
-                if (i < entries.length / 2) {
-                  return (
+                return (
+                  <>
                     <Input
                       key={i}
                       label={entry.label}
                       type={entry.type}
                       width={entry.width}
                     />
-                  );
-                } else return;
+                    {i % 3 !== 0 && i !== 7 && <div className="line" />}
+                  </>
+                );
               })}
             </div>
-            <div className="inside">
-              {entries.map((entry, i) => {
-                console.log(entry);
-                if (i >= entries.length / 2) {
-                  return (
-                    <Input
-                      key={i}
-                      label={entry.label}
-                      type={entry.type}
-                      width={entry.width}
-                    />
-                  );
-                } else return;
-              })}
-            </div>
+          </div>
+          <div className="buttons">
+            <button className="btn" onClick={""}>
+              Annuler
+            </button>
+            <button className="btn" onClick={""}>
+              Ajouter
+            </button>
           </div>
         </div>
         <div className="listeG">
---
+++ client/src/pages/gestionRelex/GestionRelex.jsx
@@ -0,0 +1,105 @@
+import Input from "../../components/form/input/Input";
+import Liste from "../../components/liste/Liste";
+import PageName from "../../components/pageName/PageName";
+import "./../../css/Gestion.css";
+const GestionRelex = () => {
+  const filterOptions = ["etat"];
+  const entries = [
+    {
+      label: "Numéro Sous Compte",
+      type: "number",
+      width: "80%",
+    },
+    {
+      label: "Désignation Sous Compte",
+      type: "text",
+      width: "80%",
+    },
+    {
+      label: "Motif déplacement",
+      type: "text",
+      width: "80%",
+    },
+    {
+      label: "Montant engagé",
+      type: "number",
+      width: "80%",
+    },
+    {
+      label: "Nature demande",
+      type: "text",
+      width: "80%",
+    },
+
+    {
+      label: "Observation",
+      type: "text",
+      width: "80%",
+    },
+    {
+      label: "Date de départ",
+      type: "date",
+      width: "80%",
+    },
+    {
+      label: "Date de retour",
+      type: "date",
+      width: "80%",
+    },
+  ];
+  return (
+    <div className="gestion">
+      <PageName name="gestion RELEX" />
+      <div className="elements">
+        <div className="form">
+          <div className="formTitle">
+            Formulaire d'Envoi demande de billetterie
+          </div>
+          <div className="inputs">
+            <div className="inside">
+              {entries.map((entry, i) => {
+                return (
+                  <>
+                    <Input
+                      key={i}
+                      label={entry.label}
+                      type={entry.type}
+                      width={entry.width}
+                    />
+                    {i % 3 !== 0 && <div className="line" />}
+                  </>
+                );
+              })}
+            </div>
+            <div className="outside">
+              <label className="label">Motif</label>
+              <textarea
+                className="subinput"
+                style={{ marginTop: "1em" }}
+                cols={35}
+                rows={10}
+              />
+            </div>
+          </div>
+          <div className="buttons">
+            <button className="btn" onClick={""}>
+              Annuler
+            </button>
+            <button className="btn" onClick={""}>
+              Envoyer
+            </button>
+          </div>
+        </div>
+        <div className="listeG">
+          <Liste
+            title="Liste des demandes de billetterie"
+            search={["id"]}
+            filterOptions={filterOptions}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GestionRelex;
