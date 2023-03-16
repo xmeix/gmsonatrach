018800194b660f8e3fdba6468fe92e01ddece114
xmeix <lamiaboualouache@gmail.com>
2023-03-16 12:24:31 +0100

    cors issue

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 const corsOptions = {
   credentials: true,
-  origin: "http://127.0.0.1:5173",
+  origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
 };
 
 app.use(cors(corsOptions));
